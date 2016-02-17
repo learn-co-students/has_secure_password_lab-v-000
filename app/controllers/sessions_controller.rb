3b5024966c83eabf80ff9ba1de1adb0ccc8c5962
Shana L Moore <shana.lavina.moore@gmail.com>
2016-02-17 12:40:25 -0800

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to '/homepage'
     else
-      redirect_to login_path
+      redirect_to '/users/new'
     end
   end
 
