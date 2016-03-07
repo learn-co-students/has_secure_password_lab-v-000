61b9b768c91efd9cb618fef912649e37b0178940
eric-an <erican@me.com>
2016-03-07 14:28:17 -0800

    define authentication flow using bcrypt gem

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(name: params[:user][:name])
+    if user && user.authenticate(params[:user][:password])
+      session[:user_id] = user.id
+      redirect_to welcome_path
+    else  
+      redirect_to new_session_path
+    end  
+  end
+end
