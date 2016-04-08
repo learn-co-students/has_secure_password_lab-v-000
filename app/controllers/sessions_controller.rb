b6f2114d5ae3b0803edaf656861c0f3af3afb7fb
authorbeard <msnolan@gmail.com>
2016-04-08 18:29:11 -0400

    began sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user=User.find_by(params[:username])
+    if user && user.authenticate(params[:password])
+      login(user.id)
+      current_user
+
+
   end
 
   def destroy
+    session.delete[:user_id]
   end
 end
