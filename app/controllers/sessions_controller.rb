e93f753f1c4c0fe38a7c37a94b83835b7b5ff52a
the-widget <brennenawana108ny@gmail.com>
2016-04-13 20:23:41 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @user = User.find_by(name: params[:user][:name])
+    if @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      redirect_to new_session_path
+    end    
+  end
+
+end
+
