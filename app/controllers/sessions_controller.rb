4f4834a9db24fdfc10428c9288535427ee5aecb9
bacitracin <tracetrace@gmail.com>
2016-04-17 09:53:22 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(name: params[:user][:name])
+    if user && user.authenticate(params[:user][:password])
+      log_in(user)
+      redirect_to welcome_path
+    else
+      flash[:alert] = "Invalid email/password combination."
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    @current_user = nil
+    redirect_to login_path
+  end
+
+    def log_in(user)
+    session[:user_id] = user.id
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in(@user)
+      redirect_to welcome_path
+    else
+      flash[:alert] = "Password confirmation does not match. Please try again."
+      redirect_to signup_path
+    end
+  end
+
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :password, :password_confirmation)
+    end
+
+end
