8f3e80a577d87de65aa8236ca817f7ef1b299691
kylegraydev <kylegraydev@gmail.com>
2016-05-13 12:24:09 -0700

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+    def new
+    @user = User.new
+  end
+
+  def create
+    if params[:user][:password] != params[:user][:password_confirmation]
+      redirect_to new_user_path
+    else
+      @user = User.create(user_params)
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    end
+  end
+
+  def show
+    @username = User.find(session[:user_id]).name
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+
+end
