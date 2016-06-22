c9dbcce36582a2c7fe7a8d9e8fa041e188bdc4ad
Drew Pham <drewvpham@gmail.com>
2016-06-22 14:51:11 +0000

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+
+  def new
+  end
+
+  def create
+ @user = User.new(user_params).save
+  end
+
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+end
