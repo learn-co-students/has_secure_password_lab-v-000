6fcfd7902be3591d545cbcacb99a042bafffa8d6
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-07-05 19:48:52 +0000

    done

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  skip_before_action :logged_in?, only: [:new, :create]
+  
+  def show
+    @user = User.find(session[:user_id])
+    return head(:forbidden) unless current_user == @user
+  end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      redirect_to new_user_path
+    end
+  end
+    
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def login_logout_links
+    if session.include? :user_id
+      form_tag sessions_destroy_path do
+        submit_tag "Log Out"
+      end
+    else
+      link_to "Log In", new_session_path
+    end
+  end
 end
