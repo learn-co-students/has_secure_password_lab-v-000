89500aafd4c9649c3c475e0eb4ee4179581495a5
kaileeagray <kaileeagray@gmail.com>
2016-08-18 12:00:30 -0500

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+require 'pry'
+
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def login
+  end
+
+  def create
+    user = User.find_by(name: params[:user][:name])
+    if user.authenticate(params[:user][:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      flash[:notice] = "Login failed"
+      redirect_to '/login'
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  resources :users
+  resources :sessions
+
+  get '/login' => "sessions#login"
+  post '/login' => "sessions#create"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
