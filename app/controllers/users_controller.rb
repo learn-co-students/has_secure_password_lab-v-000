eb7183a0a774ec9790a816a011020836b444af79
sensei100 <jenanton01@gmail.com>
2016-08-21 09:26:02 -0400

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create 
+    if params[:user][:password] == params[:user][:password_confirmation]
+      @user = User.create(user_params)
+      session[:user_id] = @user.id
+      redirect_to '/users/show'
+    else
+      redirect_to '/'
+    end
+  end
+
+  def show
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+    else
+      redirect_to "/login"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+
+end
