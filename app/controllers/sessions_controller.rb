8656378052556cf168e5baff325a346c1befcb08
sensei100 <jenanton01@gmail.com>
2016-08-21 18:44:07 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,15 @@
 class SessionsController < ApplicationController
 
-  def create
-    @user = User.find_by(name: params[:name])
-    return head(:forbidden) unless @user.authenticate(params[:user][:password])
-    session[:user_id] = @user.id 
+  def new
+    @user = User.new
   end
 
+  def create 
+    @user = User.find_by(name: params[:session][:name])
+    return head(:forbidden) unless @user.authenticate(params[:session][:password])
+    session[:user_id] = @user.id 
+    redirect_to '/users/show'
+  end
+
+ 
 end
