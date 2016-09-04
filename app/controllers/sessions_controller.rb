41dab705c55debce86a721429aada65e83e847b9
gabe <gj+code@mail.co.de>
2016-09-04 16:09:25 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,10 @@
+class SessionsController < ApplicationController
+  def create
+    @user = User.find_by(name: params[:user][:name])
+    @user = @user.try(:authenticate, params[:user][:password])
+    return redirect_to login_path unless @user
+
+    session[:user_id] = @user.id
+    redirect_to '/'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,15 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create', as: 'start_session'
+  post '/logout' => 'sessions#destroy'
+
+  get 'users/new'
+  post '/users' => 'users#create'
+
+  root 'welcome#home'
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
