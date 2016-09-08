ae9f349c7023342840a02f209879a30ccda708de
jwolfe890 <jwolfe890@gmail.com>
2016-09-08 09:43:02 -0400

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+
+
+def new
+  @user = User.new
+end 
+
+  def create
+    user = User.new(user_params).save
+  end
+ 
+  private
+ 
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+end
