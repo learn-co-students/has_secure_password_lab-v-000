cbe9e88dee581fbfb2f30030de5242ad364a012f
jwolfe890 <jwolfe890@gmail.com>
2016-09-09 13:02:35 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,15 +5,12 @@ def new
 end 
 
 def create
-
-  @user = User.find(username: params[:username])
-  user = user.try(:authenticate, params[:user][:password])
-  redirect_to(controller: 'sessions', action: 'new') unless user
-
-  session[:user_id] = @user.id
-  @user = user
-  redirect_to controller: 'welcome', action: 'home'
-
+    user = User.find_by(name: params[:user][:name])
+    user = user.try(:authenticate, params[:user][:password])
+    return redirect_to(controller: 'sessions', action: 'new') unless user
+    session[:user_id] = user.id
+    @user = user
+    redirect_to 'users/home'
 end 
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,14 @@
 class UsersController < ApplicationController
 
-
   def new
     @user = User.new
   end 
 
   def create
-    binding.pry
     @user = User.create(user_params)
     return redirect_to controller: 'users', action: 'new' unless @user.save
     session[:user_id] = @user.id
-    binding.pry
-    redirect_to 'home'
+    render 'users/home'
   end
  
   private
