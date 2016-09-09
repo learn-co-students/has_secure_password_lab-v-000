9609ec171cba405919a044ddb8409b2a822d0951
jwolfe890 <jwolfe890@gmail.com>
2016-09-09 12:01:29 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
 
+def new
+  @user = User.new
+end 
+
+def create
+
+  @user = User.find(username: params[:username])
+  user = user.try(:authenticate, params[:user][:password])
+  redirect_to(controller: 'sessions', action: 'new') unless user
+
+  session[:user_id] = @user.id
+  @user = user
+  redirect_to controller: 'welcome', action: 'home'
+
+end 
 
 end
+
+
+
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,17 @@
 class UsersController < ApplicationController
 
 
-def new
-  @user = User.new
-end 
+  def new
+    @user = User.new
+  end 
 
   def create
-    user = User.new(user_params).save
+    binding.pry
+    @user = User.create(user_params)
+    return redirect_to controller: 'users', action: 'new' unless @user.save
+    session[:user_id] = @user.id
+    binding.pry
+    redirect_to 'home'
   end
  
   private
@@ -14,4 +19,5 @@ end
   def user_params
     params.require(:user).permit(:name, :password, :password_confirmation)
   end
+  
 end
