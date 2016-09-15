f3a729c2e600cf989c6d017da61500837b9723bb
jbok4 <jbok4@aol.com>
2016-09-14 20:50:17 -0400

    controllers and user model setup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+
+  def create
+    user = User.new(user_params).save
+  end
+
+  private
+ 
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+end
