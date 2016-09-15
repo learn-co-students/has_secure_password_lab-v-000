f0c953b5c009aa408eaeaf906fb2bd84d81ab1b3
jbok4 <jbok4@aol.com>
2016-09-14 23:28:46 -0400

    routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
 
+  def new
+    @user = User.new
+  end
+
   def create
     user = User.new(user_params).save
+    redirect_to root_path
   end
 
   private
