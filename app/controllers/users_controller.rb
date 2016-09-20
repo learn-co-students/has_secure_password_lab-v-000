2c441b40ff066184f1503ea044ae007dd2585f8d
CodeByLine <yleventhal@ymail.com>
2016-09-20 17:09:10 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+
+  def index
+    @user = (User.find_by(id: session[:user_id]) || User.new)
+  end
+
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to 'users/index'
+    else
+      redirect_to signup_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
