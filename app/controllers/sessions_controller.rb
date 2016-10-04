b194c5d46c10b3fbd55fc43a31c0720b49915da9
auranbuckles <auran59@gmail.com>
2016-10-04 01:09:41 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,20 @@ class SessionsController < ApplicationController
     end
   end
 
+  # both create methods below work
+
+  # def create
+  #   user = User.find_by(name: params[:user][:name])
+  #   user = user.try(:authenticate, params[:user][:password])
+  #   return redirect_to(controller: 'sessions', action: 'new') unless user
+  #   session[:user_id] = user.id
+  #   @user = user
+  #   redirect_to controller: 'welcome', action: 'home'
+  # end
+
   def create
-    @user = User.find_by(name: params[:name])
-    if !@user.nil? && @user.authenticate(params[:password])
+    @user = User.find_by(name: params[:user][:name])
+    if !@user.nil? && @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
       redirect_to '/welcome'
     else
