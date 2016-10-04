03b99c0e837904f9aa2372c1f5f626ed4fcaee74
auranbuckles <auran59@gmail.com>
2016-10-04 01:07:33 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+    if !current_user.nil?
+      redirect_to '/welcome'
+    end
+  end
+
+  def create
+    @user = User.find_by(name: params[:name])
+    if !@user.nil? && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to '/welcome'
+    else
+      redirect_to '/login'
+    end
+  end
+
+  def destroy
+    session.delete :user_id
+    redirect_to '/login'
+  end
+
+end
