df71cf64f67c7ec32f4071eda80f08f6c5cd7e18
DownsideupmediaUSA <brandondecarlo@live.com>
2016-10-20 13:53:36 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  root 'welcome#hello'
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
 
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout'=> 'sessions#destroy'
+  resources :users
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
---
+++ myoutline.md
@@ -0,0 +1,12 @@
+I. Create Models
+  A.User (name, password digest, has secure password)
+
+
+II. Create Controllers
+  A.Users - new, create methods
+    1.views
+      a)new, show
+  B.Sessions - set new password, authenticate methods
+  C.Welcome -
+    1.views
+     a)index(homepage after successful login)
