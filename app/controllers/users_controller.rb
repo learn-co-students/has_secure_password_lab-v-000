8029c23a03e47878474f5f3721f2256566952dde
Bob Habig <rfhab2@gmail.com>
2016-12-18 17:38:34 -0500

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,16 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
-    @user = User.create(user_params)
-    session[:user_id] = @user.id
-    if @user.authenticate(params[:user][:password])
-      redirect_to '/'
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to home_path(@user)
     else
-      redirect_to new_user_path
+      redirect_to '/users/new'
     end
   end
 
@@ -17,12 +18,13 @@ class UsersController < ApplicationController
   end
 
   def home
+    @user = User.find(params[:id])
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :password, :password_comfirmation)
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 
 end
