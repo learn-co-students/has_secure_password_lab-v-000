bac50004443a767972b6234f8bf7ecc6714ace2f
neener <janinemrosen@gmail.com>
2017-01-16 21:54:56 -0500

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+  	user = User.new(user_params).save
+  end
+
+  private
+
+  def user_params
+  	params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+end
