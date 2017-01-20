3db873599f071cf5e83c5979df7232fec77c5c3a
neener <janinemrosen@gmail.com>
2017-01-20 01:53:25 -0500

    redo

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   post'/login' => 'sessions#create'
   
 
-  get 'welcome/home'
+  root 'welcome#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
