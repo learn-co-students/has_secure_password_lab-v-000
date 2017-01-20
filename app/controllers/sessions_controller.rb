2aa31f39137cad8efe4acb80b338829ee7317771
Bill <storywr@gmail.com>
2017-01-20 17:45:27 -0500

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,13 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @user = User.find_by(username: params[:username])
+    return head(:forbidden) unless @user.authenticate(params[:password])
+    session[:user_id] = @user.id
+    redirect_to controller: 'welcome', action: 'home'
+  end
+
+end
