7107e96ec57f5b40453078b48dd2e10daecf9b30
christinechoi <smi77y@gmail.com>
2017-02-09 21:32:49 -0500

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170210022806) do
+ActiveRecord::Schema.define(version: 20170210023225) do
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
+    t.string "name"
     t.string "password_digest"
   end
 
