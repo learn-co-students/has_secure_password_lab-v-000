04d6c9b1f53e31d2cccd1904ea800030aed5dfc9
Malind Kowalski <malind.nikulski@gmail.com>
2017-02-21 16:22:37 -0700

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(name: params[:user][:name])
+    if user.try(:authenticate, params[:user][:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      redirect_to login_path, alert: "Invalid"
+    end
+  end
+end
