1a073beff87d7aec043e11f109e8da508d6a85ff
Michael Djurdjevic <mdjurdj1@binghamton.edu>
2017-03-15 19:52:24 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -21,9 +21,5 @@ class SessionsController < ApplicationController
     redirect_to root_path
   end
 
-  private
-  def user_params
-    params.require(:user).permit(:name, :password)
-  end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,17 +5,18 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
+    if @user.save 
       session[:user_id] = @user.id
       redirect_to root_path
     else
       flash[:notice] = @user.errors.full_messages.uniq
-      render :new
+      redirect_to new_user_path
     end
   end
 
   private
   def user_params
-    params.require(:user).permit(:name, :password)
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
+
 end
