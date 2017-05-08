c2b8c466e96f08956d87217f09bcba149233163a
Trai Compton <tlcompton2546@outlook.com>
2017-05-08 22:12:37 +0000

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.valid?
+      @user.save
+      session[:user_id] = @user.id
+      redirect_to(controller: 'users', action: 'show')
+    else
+      redirect_to(controller: 'users', action: 'new')
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+end
