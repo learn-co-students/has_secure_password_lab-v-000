c5fce82854dcd996c9117c0d61a18032f611273f
Howard DeVennish <howarddevennish@gmail.com>
2017-05-12 16:58:02 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(name: params[:name])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
+      binding.pry
       redirect_to welcome_path
     else
       render :new
