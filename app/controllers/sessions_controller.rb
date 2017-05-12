2159dd252a6adc4d55eb5ce94179eaa8208ef816
Howard DeVennish <howarddevennish@gmail.com>
2017-05-12 17:29:22 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(name: params[:name])
-    if @user && @user.authenticate(params[:password])
+    @user = User.find_by(name: user_params[:name])
+    if @user && @user.authenticate(user_params[:password])
       session[:user_id] = @user.id
-      binding.pry
       redirect_to welcome_path
     else
       render :new
@@ -17,6 +16,12 @@ class SessionsController < ApplicationController
   def destroy
     session.clear
     redirect_to login_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password)
   end
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,11 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
     if @user.save
-      #log them in and welcome user
       session[:user_id] = @user.id
       redirect_to welcome_path
     else
-      render :new
+      redirect_to new_user_path
     end
   end
 
@@ -27,7 +25,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.requre(:user).permit(:name, :password, :password_confirmation)
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 
 end
