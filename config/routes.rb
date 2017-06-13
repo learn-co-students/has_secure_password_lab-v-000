64819fd4fec50866af0efdc8423f14d8f28d258e
github username <github email address>
2017-06-13 12:07:20 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,19 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
+    get '/login' => 'sessions#new'
+    post '/login' => 'sessions#create'
+    post '/logout' => 'sessions#destroy'
+
+    resources :users
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+
+
 
   # Example resource route with options:
   #   resources :products do
