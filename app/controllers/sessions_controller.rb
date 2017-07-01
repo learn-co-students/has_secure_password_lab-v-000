235f861c1818b07fe2f851c033746c32828dd2f7
Erika Hughes <ehughes@knights.ucf.edu>
2017-07-01 00:27:16 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ require 'pry'
 class SessionsController < ApplicationController
 
   def create
-    binding.pry
+    # binding.pry
     @user = User.find_by(name: params[:user][:name])
     return head(:forbidden) unless @user.try(:authenticate, params[:user][:password])
     session[:user_id] = @user.id
