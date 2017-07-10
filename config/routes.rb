c1167fb7010a006f940fcac91d154fafce9e10ff
r0tiart <antley.chu@gmail.com>
2017-07-10 18:06:41 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  resources :sessions, only: [:new, :create]
+  resources :users, only: [:new, :create, :index]
+
 end
