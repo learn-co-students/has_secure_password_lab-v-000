e69b77cef9c49a661ef4f52bbfb59ee9709e4451
Jdville03 <jarrel.delottinville@gmail.com>
2017-07-21 04:45:57 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,12 @@
 class SessionsController < ApplicationController
   def new
-    @user = User.new
   end
 
   def create
-    @user = User.find_by(name: params[:user][:name])
-    if @user.try(:authenticate, params[:user][:password])
-      session[:user_id] = @user.id
-      redirect_to user_path(@user)
+    user = User.find_by(name: params[:user][:name])
+    if user.try(:authenticate, params[:user][:password])
+      session[:user_id] = user.id
+      redirect_to root_path
     else
       redirect_to login_path, alert: "Incorrect password. Try again."
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  root 'welcome#home'
+
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   post 'logout' => 'sessions#destroy'
