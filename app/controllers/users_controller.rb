23ca2d1ee07e109fe102c60439f4c92604051569
Travis Cole <iamtraviscole@gmail.com>
2017-08-03 21:29:46 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,6 @@
 class UsersController < ApplicationController
 
   def index
-    if current_user
-      @user = current_user
-    else
-      redirect_to users_new_path
-    end
   end
 
   def new
