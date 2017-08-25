7a14741ea261791e7ea2e1d5e6b6b5716b9cdb29
CLackrone <charisaanne@gmail.com>
2017-08-25 10:10:25 -0400

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+
+  def new
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  def create
+    if params[:user][:password] == params[:user][:password_confirmation]
+      @user = User.create(user_params)
+      session[:user_id] = @user.id 
+      redirect_to "/users/#{@user.id}"
+    else
+      redirect_to new_user_path
+    end
+  end
+ 
+  private
+ 
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+
+end
