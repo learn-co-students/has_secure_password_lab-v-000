986673c710ef047ad445763220aa7ee66c97d257
James Hotchkiss <jimbohotchkiss@yahoo.com>
2017-09-25 14:47:46 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,18 +4,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to 'root_path'
-    else
-      redirect_to '/new'
-    end
+    @user = User.new(user_params).save
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :password_digest, :password_confirmation)
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 
 end
