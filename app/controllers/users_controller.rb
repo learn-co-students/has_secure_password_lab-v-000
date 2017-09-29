0a5b4d3ca211d4f92e980e402a86c2c85b376879
psnider30 <pbsnider@gmail.com>
2017-09-29 11:59:41 -0700

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+
+  def welcome
+    require_logged_in
+  end
+
+  def new
+    if current_user
+      redirect_to root_path
+    else
+      @user = User.new
+    end
+  end
+
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save && params[:user][:password] == params[:user][:password_confirmation]
+      session[:user_id] = @user.id
+      render :welcome
+    else
+      redirect_to signup_path
+    end
+  end
+
+end
