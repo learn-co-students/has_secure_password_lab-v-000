97716f27cd1b243fc09fff7f2213ce237cfec447
laramontana <larabrightside@gmail.com>
2017-10-26 12:38:25 +0300

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,24 @@
 class UsersController < ApplicationController
 
   def new
-    #renders a signup form
+    @user = User.new
+    render :signup
   end
 
   def create
-    #creates a new user
+    @user = User.new(user_params)
+    if @user.save
+      render :welcome
+    else
+      render :signup
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 
 end
