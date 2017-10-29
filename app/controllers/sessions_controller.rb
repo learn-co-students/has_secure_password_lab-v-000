8291ae672c7e8501060670e2e10f83ab10ffc3b2
Nicholas D'Amico <nickalan82@icloud.com>
2017-10-29 11:17:31 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,12 @@
 class SessionsController < ApplicationController
-	before_action :require_login, only: :destroy
+	
 	def new
 		@user = User.new		
 	end
 
 	def create
 		@user = User.find_by(name: params[:user][:name])
-		if @user.authenticate(params[:user][:password])
+		if user = @user.try(:authenticate, params[:user][:password])
 			session[:user_id] = @user.id
 			redirect_to '/'
 		else	
@@ -19,12 +19,4 @@ class SessionsController < ApplicationController
 		redirect_to login_path
 	end
 
-	private	
-	def session_params
-		params.require(:user).permit(:name, :password)
-	end
-
-	def require_login
-		return head(:forbidden) unless session.include? :user_id
-	end
 end
