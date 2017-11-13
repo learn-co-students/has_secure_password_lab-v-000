37263197c28478ad7ef8d6bfd303449bbe406f5c
Kevin Yongchul Kim <kevikim33@gmail.com>
2017-11-12 23:12:46 -0800

    user controller working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to users_path
+    else
+      flash[:alert] = "Invalid username/password"
+      redirect_to new_user_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+
 end
