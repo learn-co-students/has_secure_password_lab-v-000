43c0ccad7c3b7e146bb857c2b39d9c5c1e6b42b9
johanesteves <jsesteves91@gmail.com>
2017-11-18 23:07:36 -0500

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(name: params[:user][:name])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to welcome_path
+    else
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    if session.include? :user_id
+      session.delete :user_id
+    end
+    redirect_to login_path
+  end
+
+end
