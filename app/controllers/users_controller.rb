925a39027f44dcc828cbd02f21a6dc6d92ab5064
Annette Maza <nettymaza@Annettes-MacBook.local>
2017-11-21 15:39:55 -0700

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,12 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
-    return redirect_to controller: 'users', action: 'new' unless @user.save
-    session[:user_id] = @user.id
-    redirect_to controller: 'welcome', action: 'home'
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to controller: 'welcome', action: 'home'
+    else
+      redirect_to controller: 'users', action: 'new'
+    end
   end
 
   private
