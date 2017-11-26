7e33cf573053506860d9f24fa2c694814d019213
ndalcin <dalcin.nicole@gmail.com>
2017-11-26 12:34:46 -0500

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(name: params[:user][:name])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to welcome_path
+    else
+      flash.now[:danger] = 'Invalid name/password combination'
+      redirect_to login_path
+    end
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  root 'sessions#new'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get  '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+  get '/welcome' => 'welcome#homepage'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
