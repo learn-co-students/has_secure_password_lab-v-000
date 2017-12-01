6367bdfd69e081fc9c871e2dffe706f3f6312276
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-12-01 15:45:41 -0500

    Created new user flow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,33 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
 
   def new
-    
+    @user = User.new
   end
 
   def create
-
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      flash[:notice] = "Error Creating Account. Please Try Again"
+      render "users/new.html.erb"
+    end
   end
 
+  def show
+    binding.pry
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find_by(id: session[:user_id])
+  end
 end
