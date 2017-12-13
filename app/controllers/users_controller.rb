8a8db06642907f220c832351495a0f7d8801d8c1
Jacqlene Sifre <jsifre01@gmail.com>
2017-12-13 01:33:10 +0000

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+
+  def welcome
+  end
+  
+  def new
+    @user = User.new(user_params)
+  end
+
+  def create
+    if params[:user][:password] == params[:user][:password_confirmation]
+      @user = User.create(user_params)
+      session[:user_id] = @user.id
+      redirect_to new_user_path
+    else
+      redirect_to new_user_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+
+end
