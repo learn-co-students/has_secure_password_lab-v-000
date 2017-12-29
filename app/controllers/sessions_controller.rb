3c1a3d227b5f673109002a290b163e03909a1ce3
chrisjamesr <chrisjames.r@gmail.com>
2017-12-29 18:19:45 -0500

    add alternate solution to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-    
+    # @user = User.find_by(name: params[:user][:name])
+    # return head(:forbidden) unless @user.authenticate(params[:user][:password])
+    # session[:user_id] = @user.id
     user = User.find_by(name: params[:user][:name])
     if user && user.authenticate(params[:user][:password])
       session[:user_id] = user.id
