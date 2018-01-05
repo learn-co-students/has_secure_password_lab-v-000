15e700eef41498de234f3a1eae69c09c13b37f3f
Jessica Bensch <jessica.bensch11@gmail.com>
2018-01-05 14:55:49 -0500

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+
+  def create
+    if User.find_by(name: params[:user][:name])
+      @user = User.find_by(name: params[:user][:name])
+      return head(:forbidden) unless @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to '/welcome'
+    else
+      redirect_to '/user/new'
+    end
+  end
+
+end
