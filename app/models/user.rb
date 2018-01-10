a5fa673500b22f443099712169e90e935e603b7d
Nour Kodeih <nour.kodeih@gmail.com>
2018-01-10 18:23:37 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
   validates :password, presence: true
-  # validates :password_confirmation, presence: true
+
   validates_confirmation_of :password
-  # validate :passwords_must_match
-  #
-  # def passwords_must_match
-  #   :password == :password_confirmation
-  # end
+
 end
