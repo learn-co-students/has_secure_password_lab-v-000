99febbb37d3bd38ee2883979ad034b70c8ac87c1
Ary Baldioceda <abaldioceda@gmail.com>
2018-01-30 22:27:16 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,36 @@
+class SessionsController < ApplicationController
+  
+  def index
+    if !!session[:user_id]
+      @user = User.find_by(id: session[:user_id])#some code and then render :show
+    else
+      redirect_to new_session_path
+    end
+  end
+  
+  #get /login
+  def new
+    
+  end
+  
+  #post /login
+  def create
+    if @user = User.find_by(name: params[:user][:name])
+      return head(:forbidden) unless @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      redirect_to new_session_path
+    end
+  end
+  
+  def destroy
+    if session.include? :user_id
+      session.delete :name
+      redirect_to new_session_path
+    else
+      redirect_to new_session_path
+    end
+  end
+  
+end
