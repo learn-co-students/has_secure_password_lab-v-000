0b03f1dee8a117429cb2efbe2a34816bd90fa23f
Anastasia  Barkett <anastasia.barkett@gmail.com>
2018-02-28 21:57:43 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
-    return direct_to controller: 'users', action: 'new' unless @user.save
+    return redirect_to controller: 'users', action: 'new' unless @user.save
     session[:user_id] = @user.id
     redirect_to controller: 'welcome', action: 'home'
   end
