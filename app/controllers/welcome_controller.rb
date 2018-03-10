6379da3f5de7ef1cd0e7f347db9ac31810069720
github username <github email address>
2018-03-09 21:19:15 -0500

    Done.

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,11 @@
+class WelcomeController < ApplicationController
+
+  def home
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+    else
+      redirect_to login_path
+    end
+  end
+
+end
