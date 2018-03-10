3f5e00d1a36f804b02afac9a7a3f6162edc0bd93
github username <github email address>
2018-03-09 22:14:56 -0500

    refactor

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,19 +4,21 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if user = User.find_by(name: params[:user][:name])
-      user = user.try(:authenticate, params[:user][:password])
-      return redirect_to(controller: 'sessions', action: 'new') unless user
-      session[:user_id] = user.id
-      @user = user
-      redirect_to controller: 'welcome', action: 'home'
-    else
-      redirect_to new_user_path
-    end
+    user = User.find_by(name: params[:user][:name])
+
+    user = user.try(:authenticate, params[:user][:password])
+
+    return redirect_to(controller: 'sessions', action: 'new') unless user
+
+    session[:user_id] = user.id
+
+    @user = user
+
+    redirect_to controller: 'welcome', action: 'home'
   end
 
   def destroy
-    session.destroy
-    redirect_to controller: 'sessions', action: 'new'
+    session.delete(:user_id)
+    redirect_to root
   end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,7 @@
 class WelcomeController < ApplicationController
+  before_action :require_logged_in
 
   def home
-    if session[:user_id]
-      @user = User.find(session[:user_id])
-    else
-      redirect_to login_path
-    end
   end
 
 end
