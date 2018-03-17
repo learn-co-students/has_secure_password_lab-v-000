3c5ba7bfa8c2bc4332647b6fd56260739f6541ae
Sherry Elle <sherryelle@Sherrys-MacBook.local>
2018-03-17 17:54:26 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,34 +1,22 @@
 class UsersController < ApplicationController
-  # def create
-  #   user = User.create(user_params)
-  #
-  #   if user.try(:authenticate, params[:user][:password])
-  #     binding.pry
-  #
-  #       session[:user_id] = user.id
-  #     render :index
-  #   else
-  #     redirect_to '/users/new'
-  #   end
 
-def create 
-   @user = User.new(name: params[:user][:name], password: params[:user][:password], password_confirmation: params[:user][:password_confirmation]) 
-     if @user && @user.password == @user.password_confirmation 
-       @user.save 
-       flash[:success] = "You have successfully signed up and logged in" 
-       session[:user_id] = @user.id 
-       # welcome 
-     else 
-       # flash[:error] = "You must signup before logging in" 
-       redirect_to '/users/new' 
-     end 
-   end
+  def new
+  end
 
+  def create
+    user = User.create(user_params)
+
+    if user.try(:authenticate, params[:user][:password])
+      session[:user_id] = user.id
+      redirect_to '/users/new'
+    else
+      redirect_to '/users/new'
+    end
+  end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :password, :password_confirmation)
   end
-
 end
