9fc26b2fa8ef3180ff4d6d7201b7fd9a07432083
Adjoa <adjoa.darien@gmail.com>
2018-03-21 05:06:45 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  
+  root 'users#home'
+  resources :sessions, only: [:create, :destroy]
+
+  get 'users/new'
+  post 'users/create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
