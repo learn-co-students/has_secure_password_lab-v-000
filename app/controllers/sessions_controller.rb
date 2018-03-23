5934405eeaa424b59acfe80f0c63c920f4c470a2
independnt <gonzalezdennis6@gmail.com>
2018-03-23 15:41:14 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  before_action :logged_in?
+  #before_action :logged_in?
 
   def new
     @user = User.new
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,8 @@
 class WelcomeController < ApplicationController
-  before_action :logged_in?
+  #before_action :logged_in?
+
   def welcome
     @user = User.find_by(id: session[:user_id])
   end
+
 end
