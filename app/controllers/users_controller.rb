cbf3980d7a31ec0e3dc4605ef7033cad194e2cda
Chris Henry <cdhenry@gmail.com>
2018-04-06 20:06:25 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,12 @@ class UsersController < ApplicationController
   end
 
   def create
+    user = User.new(user_params).save
+  end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :user, only: [:new, :create]
+  root 'application#hello'
+  resources :users, only: [:new, :create]
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
