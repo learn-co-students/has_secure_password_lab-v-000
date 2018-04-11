0df3e15d3bedba8ef1f0e13bbb9943b9a156fddb
kostanzhoglo <jeffreywithers@gmail.com>
2018-04-11 14:36:30 -0400

    Add has_secure_password functionality to app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+root "welcome#index"
+resources :users, only: [:new, :create]
+resources :sessions, only: [:new, :create]
+get "/home" => "welcome#home"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
