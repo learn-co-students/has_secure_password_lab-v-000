dd520968bf2e93084cdbccadb1b88c0765e6bae9
funemployeddad <funemployeddad@gmail.com>
2018-04-13 13:40:04 -0700

    all but sessions controller specs are passing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,17 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to '/users/show'
+      redirect_to new_user_path
     else
       render :new
     end
   end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :password)
+  end
+
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ActiveRecord::Base
 
   def login
   end
-  
+
 end
