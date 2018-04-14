b657604e75d1bdbf13aed5db24f7751d70ab734a
EC2 Default User <ec2-user@ip-172-31-8-108.ec2.internal>
2018-04-14 22:31:27 +0000

    Done.

--- db/migrate/20180414223052_create_users.rb
+++ db/migrate/20180414223052_create_users.rb
@@ -1,8 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :username
+      t.string :name
       t.string :password
+      t.string :password_confirmation
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180414222444) do
+ActiveRecord::Schema.define(version: 20180414223052) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "username"
+    t.string   "name"
     t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "password_confirmation"
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
   end
 
 end
