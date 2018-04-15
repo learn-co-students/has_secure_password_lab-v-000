73de4ffced0429313fd5eac78a151ff8e0497470
EC2 Default User <ec2-user@ip-172-31-8-108.ec2.internal>
2018-04-15 19:44:27 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   def create
-    user = User.new(user_params).save
+    user = User.new(user_params)
+    if !user.name || user.name.empty? || !has_secure_password
+        redirect_to '/users/new'
+    else
+        session[:user_id] = user.id
+        redirect_to '/welcome/welcome'
   end
 
   private
