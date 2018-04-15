709465e25b69ca843c40f8fb30f7396cbea83076
EC2 Default User <ec2-user@ip-172-31-8-108.ec2.internal>
2018-04-15 19:17:35 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,17 +5,20 @@ class UsersController < ApplicationController
     end
     
     def create
-        @user = User.new(name: params[:name], password: params[:password], password_confirmation: params[:password_confirmation]) 
-         if !params[:name] || params[:name].empty? || params[:password] != params[:password_confirmation]
+        @user = User.new(user_params)
+         if !@user.name || @user.name.empty? || @user.password != @user.password_confirmation
        redirect_to '/users/new'
    else
             @user.save
+            session[:user_id] = @user.id
             redirect_to '/welcome/welcome'
         end
     end
     
+
+    
     private
-    def login
-        return head(:forbidden) unless session[:user_id]
-    end
+    def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ActiveRecord::Base
+    
+    def self.authenticate(password)
+   @user = User.new(params_user)
+    @user.password == @user.password_confirmation
+    @user
 end
+
+    private
+    def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
+  end
+end
+
