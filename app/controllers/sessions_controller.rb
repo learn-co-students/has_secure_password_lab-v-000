b18b230e27b79e43d761dd55f9f5839310aae8b6
EC2 Default User <ec2-user@ip-172-31-8-108.ec2.internal>
2018-04-16 18:07:52 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
    end
 def create
     @user = User.find_by(name: params[:user][:name])
-    if @user && @user.authenticate(params[:user][:password])
+    if @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
       redirect_to welcome_welcome_path
     else
@@ -12,22 +12,5 @@ def create
     end
   end
 end
-#   def create
-#     #   binding.pry
-#     user = User.find_by(name: params["user"]["name"])
-    
-#      if !params["user"]["password"]
-#     #     session[:user_id] == nil
-#          redirect_to '/sessions/new'
-#      end
-#   user.try(:authenticate, params["user"]["password"])
-#       if user
-#     session[:user_id] = user.id
-# end
 
-   
-    # @user.try(:authenticate, params[:password])
-    # if @user
-    #   session[:user_id] = @user.id
-    # elsif !password.valid? || !password
      
