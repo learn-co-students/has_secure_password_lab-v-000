3e958e6e1af6494e53f3314bc628e79e82a417cb
Richard Chang <rchang9999@yahoo.com>
2018-04-21 19:02:57 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user= User.create(user_params)
     return redirect_to controller: 'users', action: 'new' unless @user.save
     session[:user_id]=@user.id
-    redirect_to controller: 'welcome', action: 'new'
+    redirect_to controller: 'welcome', action: 'home'
   end
 
   private
