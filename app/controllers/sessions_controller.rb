2f7979d29e7e39b7fc6d3a67446f7490c3678de6
Richard Schaubeck <richard.schaubeck@gmail.com>
2018-04-24 00:49:58 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,12 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(name: params[:user][:name])
+    @user = @user.try(:authenticate, params[:user][:password])
+    return redirect_to login_path unless @user
+    session[:user_id] = @user.id
+    redirect_to "/users/homepage"
+ end
+end
