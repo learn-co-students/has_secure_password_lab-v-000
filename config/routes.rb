cff82d1ec88f7bfd3317679c8b832203d21c924a
Benny Hudson Hung <wakenightryu@gmail.com>
2018-04-25 05:52:51 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-
-  get '/users/new' => 'users#new'
-  post '/users/new' => 'users#create'
-  get 'users/homepage' => 'users#homepage'
-  post '/users/homepage' => 'users#homepage'
-  get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
-  get '/logout' => 'sessions#destroy'
-  post '/logout' => 'sessions#destroy'
-
-end
+	  get '/users/new' => 'users#new'
+	  post '/users/new' => 'users#create'
+	  get 'users/homepage' => 'users#homepage'
+	  post '/users/homepage' => 'users#homepage'
+	  get '/login' => 'sessions#new'
+	  post '/login' => 'sessions#create'
+	  get '/logout' => 'sessions#destroy'
+	  post '/logout' => 'sessions#destroy'
+	end
