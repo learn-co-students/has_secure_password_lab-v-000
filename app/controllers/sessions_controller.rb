555fc94b34d71d2ce7e75d2e02a4c37f7875d707
AndrewHongUW <32497926+AndrewHongUW@users.noreply.github.com>
2018-04-30 11:05:04 -0700

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,18 @@
 class SessionsController < ApplicationController
-
   def new
   end
 
   def create
-    @user = User.find_by(name: params[:user][:name])
-    if @user.nil? 
-      redirect_to controller: 'sessions', action: 'new'
-    elsif @user.authenticate(params[:user][:password])
-      session[:user_id] = @user.id
-      redirect_to user_path(@user)
-    else
-      redirect_to controller: 'sessions', action: 'new'
-    end
+    user = User.find_by(name: params[:user][:name])
+    user = user.try(:authenticate, params[:user][:password])
+    return redirect_to(controller: 'sessions', action: 'new') unless user
+    session[:user_id] = user.id
+    @user = user
+    redirect_to controller: 'welcome', action: 'home'
   end
 
+  def destroy
+    session.delete :user_id
+    redirect_to '/'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -57,4 +57,6 @@ Rails.application.routes.draw do
   resources :users
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+  root 'welcome#home'
 end
