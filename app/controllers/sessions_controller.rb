d9e14f003c0ba4a0dbcd41104759dd0c2bc95977
Eden <emesfun@outlook.com>
2018-05-14 09:34:21 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,12 @@
 class SessionsController < ApplicationController
 
+    def new 
+    end
+
     def create 
         @user = User.find_by(name: params[:user][:name])
-
-        #figure out - is the @user password_confirmation supposed to be nil?
-        # I think it is due to security reasons, but google
-        # also, research how .authenticate authenticates a user password
-        
-        if @user.authenticate(params[:password])
+        return head(:forbidden) unless @user.authenticate(params[:user][:password])
             session[:user_id] = @user.id
-        else 
-            head(:forbidden)
-        end 
     end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,13 @@ class UsersController < ApplicationController
     end
 
     def create 
-        user = User.new(user_params).save
+        user = User.new(user_params)
+        
+        if user.save
+            session[:user_id] = user.id
+        else 
+            redirect_to new_user_url
+        end
     end
 
     private 
