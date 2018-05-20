e1773e36d94be7d0d598bbb58039c365cc303d85
ecssiah <ecssiah@gmail.com>
2018-05-19 21:29:19 -0500

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+
+  helper_method :login
+
+  def new
+
+  end
+
+  def create
+    user = User.find_by(name: params[:user][:name])
+    binding.pry
+
+    return head(:forbidden) unless user.authenticate(params[:user][:password])
+
+    login(user)
+
+    redirect_to '/users'
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+  get '/users' => 'users#index'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create' 
+  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
