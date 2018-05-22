15c623c89e9f92954bfca24a8ce52c4f284d8666
johnwojo <jwojo13@gmail.com>
2018-05-22 12:05:00 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,18 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.create(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      redirect_to new_user_path
+    end
   end
 
   private
 
   def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
-
 end
