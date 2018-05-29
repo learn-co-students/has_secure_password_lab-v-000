8fb1c9f9ff07a15f4a51ba13ce2f6f935c9b26d5
Susan Fowler <infopro94@gmail.com>
2018-05-29 13:18:41 -0500

    whew. lab completed all tests passing

---
+++ NOTES.md
@@ -0,0 +1,11 @@
+[x] Create a User model and migrations.
+
+[] Create a Users controller. 
+	[] It should respond to `new` with the signup form, and 
+	[] respond to `create` by creating a new user.
+
+[] Create a Sessions controller and the appropriate actions.
+
+[] Create a user homepage. You can either 
+	[] create a controller for it (a `WelcomeController` is a reasonable thing that you'll see frequently), or 
+	[] you can make it an action in the User's controller.
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,22 @@
 Rails.application.routes.draw do
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  resources :users, only: [:create, :new]
+
+  root 'welcome#home'
+
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+     
+     
+     
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
