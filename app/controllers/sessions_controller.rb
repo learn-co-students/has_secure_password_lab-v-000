a00c80ad1304757797e3ca59b650883f61dfff64
Erin Walker <erinb0317@hotmail.com>
2018-05-31 00:04:01 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(name: user_params[:name])
+    if @user
+      return head(:forbidden) unless @user.authenticate(user_params[:password])
+      current_user(@user)
+      redirect_to user_path(@user)
+    else
+      redirect_to login_path
+    end
+  end
+end
