7a69cbac3fd4c7e701d1ce390398731becfd12d9
Kim Gonzales <Kimgo589@gmail.com>
2018-06-11 18:39:07 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :current_user
+
+  def curret_user
+    session[:user_id]
+  end
+
+  def homepage
+    redirect_to login_path unless !session[:name].nil?
+  end
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.password == @user.password_confirmation
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      redirect_to new_user_path
+      flash[:notice] = "Password must match password confirmation"
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :password, :password_confirmation)
+    end
+end
