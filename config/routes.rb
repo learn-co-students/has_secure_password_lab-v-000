3d2d9f35fae01efa1699021d9f0808e0a6b968bf
github username <github email address>
2018-06-14 19:23:48 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
 
 
-    resources :users, only [:new, :create]
+    resources :users, only: [:new, :create]
     get '/signup' => 'users#new'
     get '/' => 'welcome#homepage'
 
