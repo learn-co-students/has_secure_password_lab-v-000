2a9fd33a40908340f273e45b4c3140a642b8a12b
ritchey0713 <ritchey0713@gmail.com>
2018-06-17 09:05:23 -0400

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+require 'pry'
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      #binding.pry
+      redirect_to '/home'
+    else
+      redirect_to '/signup'
+    end
+  end
+
+    def show
+      @user = User.find(session[:user_id])
+  end
+
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+
+end
