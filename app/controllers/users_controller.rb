9d7db0cf078ac36b752ccaa3dbba5f83588e7df1
Katleiah Ramos <katleiahramos@gmail.com>
2018-06-21 15:50:57 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,20 @@
 class UsersController < ApplicationController
-    
-    def home
-    end 
 
-    def new 
+    def home
+    end
+
+    def new
     end
 
     def create
-        user = User.new(user_params)
-        return redirect_to(controller: 'users', action: 'new') unless @user.save 
-        session[:user_id] = @user.user_id
-        redirect_to(controller: 'users', action: 'home')
+        @user = User.new(user_params)
+        return redirect_to(controller: 'users', action: 'new') unless @user.save
+        session[:user_id] = @user.id
+        redirect_to(controller: 'welcome', action: 'home')
     end
 
     private
- 
+
     def user_params
       params.require(:user).permit(:name, :password, :password_confirmation)
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :users
   resources :sessions
+  get '/home', to: 'welcome#home'
 end
