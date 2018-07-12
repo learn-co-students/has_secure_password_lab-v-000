14210adb2c20ffea48c62ec5957474d8fc104288
ashley3schultz <ashleyeschultz@gmail.com>
2018-07-11 18:58:59 -0700

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,6 @@
 class SessionsController < ApplicationController
 
     def create
-        #binding.pry
         @user = User.find_by(name: params[:user][:name])
         if @user
             return head(:forbidden) unless @user.authenticate(params[:user][:password]) 
