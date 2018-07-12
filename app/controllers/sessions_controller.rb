850f9f42defe4f6ec4829d5fe56e9b2941ec659a
ashley3schultz <ashleyeschultz@gmail.com>
2018-07-11 18:43:47 -0700

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,14 @@
 class SessionsController < ApplicationController
+
+    def create
+        #binding.pry
+        @user = User.find_by(name: params[:user][:name])
+        if @user
+            return head(:forbidden) unless @user.authenticate(params[:user][:password]) 
+            session[:user_id] = @user.id
+            redirect_to '/'
+        else 
+            redirect_to 'sessions/new'
+        end 
+    end 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,22 @@
 class UsersController < ApplicationController
 
+    def new 
+    end 
+
+    def create 
+        if !params[:user][:password].empty? && params[:user][:password] == params[:user][:password_confirmation]
+            @user = User.create(user_params)
+            session[:user_id] = @user.id
+            redirect_to '/'
+        else
+            redirect_to new_user_path
+        end 
+    end 
+
+    private 
+
+        def user_params
+            params.require(:user).permit(:name, :password)
+        end 
     
 end
