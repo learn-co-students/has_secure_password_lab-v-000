1ac098b386b03de79af5adea2ae0e47353da2aff
Almighty-Mose <branden.mosley1337@gmail.com>
2018-07-14 16:57:30 -0700

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @user = User.find_by(name: params[:user][:name])
+    return head(:forbidden) unless @user.authenticate(params[:user][:password])
+    session[:user_id] = @user.id
+    redirect_to 'users#show'
+  end
+
+  def destroy
+    session.delete :user_id
+
+    redirect_to '/login'
+  end
+end
