bc3d5086f47273bab40caa5f588c4ffb6087edd0
Vanessa Toro <vanetoro84@gmail.com>
2018-07-15 12:32:31 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,17 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
-      render 'create'
-    end  
+      redirect_to new_user_path
+    end
   end
+
+  def show
+    binding.pry
+  end
+
 
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   resources :sessions, only: [:create]
-  resources :users, only: [:create, :show]
+  resources :users, only: [:create, :show, :new]
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
