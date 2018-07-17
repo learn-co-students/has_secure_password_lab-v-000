b6ad5a313a45fb50591f39e81f289b768bed2bb1
alcesalces11331 <rollin.metzger@gmail.com>
2018-07-16 17:42:53 -0700

    AddColumnToUsers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,12 @@ class UsersController < ApplicationController
   end
 
   def create
+    user == User.create(user_params)
+    if !user.save
+      return redirect_to controller: 'users', action: 'new'
+    else
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    end
   end
 end
