4380a4709d03327c68b00050a9fdeed4419432f8
AlexFriedland <friedlandaj@gmail.com>
2018-08-01 23:21:58 +0200

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 20180801210616) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.string   "password"
+    t.string   "password_digest"
     t.string   "password_confirmation"
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
