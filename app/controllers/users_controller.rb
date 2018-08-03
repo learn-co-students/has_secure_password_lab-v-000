dac6fe765a844da54c1e400ba44b374be235ad5e
taryn yaeger <tsyaeger@gmail.com>
2018-08-03 21:17:31 +0000

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  
+  def new 
+    @user = User.new 
+  end
+  
+  def create 
+    @user = User.new 
+    @user.name = params[:user][:name] 
+    @user.password = params[:user][:password] 
+    if @user.save 
+      session[:user_id] = @user.id 
+      redirect_to root_path 
+    else 
+      render 'users/new' 
+    end
+  end 
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+  get '/login' => "sessions#new"
+  post '/sessions' => "sessions#create" 
+  get '/logout' => "sessions#destoy"
 
-
+  resources :users
+  
+  root 'users#new'
 end
