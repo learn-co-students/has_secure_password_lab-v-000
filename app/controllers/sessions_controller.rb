888195b1ce1afe87b9b3e3edfda0d77c62411d05
Elle Faraday <faraday.elle@gmail.com>
2018-08-06 17:08:03 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by_name(params[:name])
-    if @user && @user.authenticate(params[:password])
+    @user = User.find_by_name(params[:user][:name])
+    if @user && @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     elsif @user
