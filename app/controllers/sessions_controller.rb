1c506ae5051a4b65ed2047183a007caa8b92efa8
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-08-07 22:52:36 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,23 +1,18 @@
 class SessionsController <ApplicationController 
   
   def new 
-    @user = User.new
+     @user = User.new
   end
   
   
   def create 
-    if params[:user]
     @user = User.find_by(name: params[:user][:name])
-    @user = @user.try(:authenticate, params[:user][:password])
-    # if @user 
-      # raise params.inspect
+    if @user && @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
       redirect_to user_path(@user)
-      # end
     else
       redirect_to(controller: 'sessions', action: 'new')
     end
-    
 	 end
 	 
 
