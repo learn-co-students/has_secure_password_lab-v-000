b3e276a2df84f48179ba9a31ecc099fe07cc9cc1
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-08-07 20:07:54 +0000

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController <ApplicationController 
+  
+  def new 
+  end 
+  
+  def create 
+  end 
+  
+  private 
+  
+  def user_params
+    params.require(:user).permit(:name, :password, :password_digest)
+  end
+  
+end
