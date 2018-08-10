01d10d7faf76d56bad1211afbfd0a2b7162383c6
ElleeB <laura.trager@gmail.com>
2018-08-09 20:29:47 -0400

    Add content to users controller methods success

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,23 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new
+    @user.name = params[:user][:name]
+    @user.password = params[:user][:password]
+    @user.save
+    
+    render 'show'
   end
 
   def show
+    if logged_in?
+      render 'show'
+    else
+      redirect_to login_path
+    end
   end
 end
