78382cd9c3dedf4d462beb425ef51ac040e4bfb3
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-08-10 18:49:59 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(name: params[:user][:name]) #we find the user by matching the user input with the name.
     if @user.authenticate(params[:user][:password]) # once we find the user, we authenticate it with its password.
-      session[:user_id] = @user.id # once authenticated, we assign the user to a session.
+      session[:user_id] = @user.id # once authenticated, we assign the user to a session, that is login.
       redirect_to root_path
     else
       redirect_to new_user_path
