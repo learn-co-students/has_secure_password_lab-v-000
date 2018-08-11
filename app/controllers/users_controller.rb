a51b9c7a1e089e0ac7da196a982c9c52eb4cc6e1
Hillevi Eklöw <hillevieklow@gmail.com>
2018-08-11 02:27:33 +0000

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.new(user_params)
+    if !user.save
+      redirect_to signup_path
+    else
+      session[:user_id] = user.id
+      redirect_to homepage_path
+    end
+  end
+
+  def homepage
+    redirect_to login_path if !session[:name]
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+end
