bef4a723ec87166dc56c0134abe3ae94bffeb261
epic0448 <samuelogundiran@Yahoo.com>
2018-10-19 14:27:11 -0400

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+    
+
+    def new
+        # It should respond to new with the signup form,
+    end
+
+   
+    def create
+        # create by creating a new user.
+        User.create(user_params)
+
+    end  
+
+    def signup
+
+    end
+
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password, :password_confirmation)
+    end
+
+
+end
