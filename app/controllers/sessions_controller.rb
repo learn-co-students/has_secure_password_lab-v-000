0bcde1e999f89b643dbe75a7792d136c5962ef69
epic0448 <samuelogundiran@Yahoo.com>
2018-10-19 21:08:59 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,11 @@
 class SessionsController < ApplicationController
     
     def create
-        binding.pry
-            @user = User.find_by(name: params[:name])
+            @user = User.find_by(params[:name])
+            binding.pry
+
             return head(:forbidden) unless @user.authenticate(params[:password])
+
             session[:user_id] = @user.id
             redirect_to user_path(@user)
 
