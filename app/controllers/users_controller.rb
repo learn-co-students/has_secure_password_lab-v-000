ac92d792d7e2a1868bc9677a4ac65ff126a49242
Anu Khambete <anukhambete@gmail.com>
2018-10-26 19:50:14 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,25 @@
+require 'pry'
 class UsersController < ApplicationController
   def new
   end
 
+  def index
+  end
+
   def create
+    #binding.pry
+    @user = User.create(user_params)
+    @user.save
+    @user.authenticate(params[:user][:password])
+    redirect_to users_path
+  end
+
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 
 end
