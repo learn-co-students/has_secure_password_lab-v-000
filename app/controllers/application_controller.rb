0e3e927424fd7f5d5b4e12d16f6c6786d7578297
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-12-03 05:22:49 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def in_session?
+    !!session[:user_id]
+  end
+
+  def welcome
+    if in_session?
+      @user = User.find(session[:user_id])
+    else
+      redirect_to '/login'
+    end
+
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,22 @@
 class SessionsController < ApplicationController
+  def new
+    @user = User.new
+    redirect_to '/' if in_session?
+  end
+
   def create
     @user = User.find_by(name: params[:user][:name])
     if @user && @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
-      redirect_to user_path(@user)
+      redirect_to '/'
     else
-      redirect_to new_session_path
+      flash[:notice] = "Login failed."
+      redirect_to new_session_url
     end
+  end
+
+  def destroy
+    session.delete :user_id unless session[:user_id].nil?
+    redirect_to '/'
   end
 end
