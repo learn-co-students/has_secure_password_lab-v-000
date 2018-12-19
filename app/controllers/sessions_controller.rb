7554c58b1d9b04ce76c61874bd12d75158b4d786
github username <github email address>
2018-12-18 21:54:54 -0800

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,18 @@
 class SessionsController < ApplicationController
   def new
+    
   end
 
   def create
-    @user = User.find_by(name: params[:name])
-    if @user.authenticate(params[:password])
-      session[:user_id] = @user.id
+    user = User.find_by(name: params[:user][:name])
+    user = user.try(:authenticate, params[:user][:password])
+    return redirect_to(controller: 'sessions', action: 'new') unless user
+
+      session[:user_id] = user.id
+
+      @user = user
       redirect_to :welcome
-    else
-      redirect_to :signup
-    end
+
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,14 +6,15 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to login_path
+      session[:user_id] = @user.id
+      redirect_to :welcome
     else
       redirect_to :signup
     end
   end
 
   def welcome
-    @user = User.find(params[:name])
+
   end
 
   private
