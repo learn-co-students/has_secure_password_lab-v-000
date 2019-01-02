4c96747121eddb63499eb0515d7f8595119a9c27
Taylor Williams <taylor@thewilliams.net>
2019-01-02 23:47:54 +0000

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.valid?
+      @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      redirect_to users_new_path
+    end
+  end
+
+  def show
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+end
