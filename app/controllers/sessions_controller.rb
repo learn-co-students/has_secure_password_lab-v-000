392e8255e452db68b64117580db8febbd4efd331
Marc Mills <mdmills1990@gmail.com>
2019-01-23 10:37:22 -0500

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,17 @@
 class SessionsController < ApplicationController
+
   def create
+    user = User.find_by(name: params[:user][:name])
+    user = user.try(:authenticate, params[:user][:password])
+    return redirect_to '/login' unless user
+    session[:user_id] = user.id
+    @user = user
+    redirect_to controller: 'users', action: 'show'
   end
-  
+
+  def destroy
+    session.delete :user_id
+    redirect_to '/login'
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,23 @@
 class UsersController < ApplicationController
 
-  def create
+  def new
+    @user = User.new
   end
 
+  def create
+    @user = User.create(user_params)
+    return redirect_to controller: 'users', action: 'new' unless @user.save
+    session[:user_id] = @user.id
+    redirect_to controller: 'users', action: 'show'
+  end
+
+  def show
+    @user = User.find_by(params[:name])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
 end
