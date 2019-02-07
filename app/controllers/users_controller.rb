1a4b2790f4d69bfebcf1aad8b6f6c40668703feb
zlehmann <zlehmann@gmail.com>
2019-02-06 17:13:07 -0800

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+
+    def create
+        user = User.create(user_params)
+        session[:user_id] = user.id
+        redirect_to '/users/new'
+    end
+
+    def user_params
+        params.require(:user).permit(:name, :password, :password_confirmation)
+    end
+
 end
