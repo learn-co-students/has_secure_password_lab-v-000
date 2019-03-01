cb762081863c1fc2e1c3f27e4655779257ad12fa
Judy McGoldrick <jjeanius@verizon.net>
2019-03-01 05:59:38 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,33 +1,42 @@
+require 'pry'
+
 class UsersController < ApplicationController
 
   def new
   end
 
   def create
-     @user = User.create(user_params)
-    # if set_your_password
-      redirect_to "/users/new"
-#    @user = User.find_by(name: params[:name])
-#    return head(:forbidden) unless params[:password] == @user.password
-    session[:user_id] = @user.id
+      @user = User.create(user_params)
+      session[:user_id] = @user.id
 
-  end
+      if params[:password] && !params[:name].empty?
+        params[:password] == params[:password_confirmation]
+        @user.password = params[:password]
 
+      else
+        redirect_to "/users/new"
+      end
 
-
-private
-  def user_params
-    params.require(:user).permit(:name, :password, :password_confirmation, :password_digest)
-  end
-
-  def self.valid
-    if password !=nil && password_confirmation !=nil
-    #  session[:name] = params[:name]
-      password == password_confirmation
-
-    end
-  end
+ end
 
 
+  #    if session[:user_id] = @user.id
+  #      user_is_valid
+  #    else
 
-end
+
+ private
+   def user_params
+     params.require(:user).permit(:name, :password, :password_confirmation, :password_digest)
+   end
+
+   def user_is_valid
+     if params[:password] && params[:name].empty?
+       params[:password] = params[:password_confirmation]
+       @password = params[:password]
+     end
+   end
+
+
+
+ end
