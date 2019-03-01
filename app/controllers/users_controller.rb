734f9aad2158d10fb433591554601ba86a13efa3
Judy McGoldrick <jjeanius@verizon.net>
2019-03-01 22:23:58 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,35 +8,17 @@ class UsersController < ApplicationController
   def create
       @user = User.create(user_params)
       session[:user_id] = @user.id
-
-      if params[:password] && !params[:name].empty?
-        params[:password] == params[:password_confirmation]
-        @user.password = params[:password]
-
+      if @user.save    # params[:user][:password] && !params[:user][:password_confirmation].empty
+      params[:user][:password] == params[:user][:password_confirmation]
+       redirect_to "/"
       else
         redirect_to "/users/new"
       end
-
- end
-
-
-  #    if session[:user_id] = @user.id
-  #      user_is_valid
-  #    else
-
+    end
 
  private
    def user_params
-     params.require(:user).permit(:name, :password, :password_confirmation, :password_digest)
+     params.require(:user).permit(:name, :password, :password_confirmation)
    end
-
-   def user_is_valid
-     if params[:password] && params[:name].empty?
-       params[:password] = params[:password_confirmation]
-       @password = params[:password]
-     end
-   end
-
-
 
  end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates :name, presence: true
-  validates :password, presence: true
-  validates :password_confirmation, presence: true
+
+#  validates :name, presence: true
+#  validates :password, presence: true
+#  validates :password_confirmation, presence: true
 
 end
