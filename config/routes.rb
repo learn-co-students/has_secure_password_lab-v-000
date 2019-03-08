15f60a86abdf4e08e2e63c245ffaabb6435f20dc
MargaritaKhodzka <mkhodzka@gmail.com>
2019-03-07 22:46:56 -0500

    User model and migrations, started working on controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'welcome/home'
+
+  get 'sessions/new'
+
+  get 'sessions/destroy'
+
+  get 'users/new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
