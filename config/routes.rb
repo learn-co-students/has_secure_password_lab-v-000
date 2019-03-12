461a73e62d2ded170a66ab257370f4718ee282e5
Rachel Wyatt <rw126@evansville.edu>
2019-03-12 12:54:28 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :users
+  get '/', to: "sessions#index", as: "root"
+  get '/login', to: "sessions#new", as: "login"
+  post '/sessions', to: "sessions#create"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
