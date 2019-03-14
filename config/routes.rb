003564338f73b1baec5bafef90c9fc4c98b75143
dani8439 <dani8439@aol.com>
2019-03-14 12:41:59 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+
+  root "application#hello"
+
+  resources :users, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
