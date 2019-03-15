78470dbcf91261f50ff41703c608a54c20824ba9
nmarini <nmarini44@gmail.com>
2019-03-15 11:56:56 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  root 'welcome#homepage'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+
+  post '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  resources :users, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
