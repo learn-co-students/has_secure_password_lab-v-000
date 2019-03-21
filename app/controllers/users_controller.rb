b3b7c9a2dee8db32dc50aaaab980fc3e4eef4265
Erin Green <erin.y.green@gmail.com>
2019-03-21 20:29:53 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,21 @@ class UsersController < ApplicationController
   end
 
   def create
-
+    User.create(user_params)
   end
 
   def homepage
-
+    if current_user
+      render(controller: 'users', action:'homepage')
+    else
+      redirect_to(controller: 'users', action: 'login')
+    end
   end
+
+  private
+
+ def user_params
+   params.require(:user).permit(:username, :password, :password_confirmation)
+ end
 
 end
