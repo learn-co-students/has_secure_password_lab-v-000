17b518370aa78e2a324adeceb9ba2b2813146255
catherinism <catcreates13@gmail.com>
2019-04-04 21:14:41 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
+  def index
+  end
+
   def new
   end
+
+  def create
+    user = User.find_by(name: params[:user][:name])
+    authenticated = user.try(:authenticate, params[:user][:password])
+    return redirect_to create_path unless authenticated
+    session[:user_id] = user.id
+    @user = user
+    redirect_to users_path
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,19 @@ class UsersController < ApplicationController
   end
 
   def create
-
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id]= @user.id
+      redirect_to create_path
+    else
+      redirect_to new_path
+    end
   end
+
+  private
+
+  def user_params
+   params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+
 end
