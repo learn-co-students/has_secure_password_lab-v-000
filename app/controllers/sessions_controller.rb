8b97b8b7a07ef6b8e78fada7b246f00a149d97f4
yuanxizhang <yuanxi.zhang@gmail.com>
2019-04-08 21:43:51 -0700

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,19 +4,6 @@ class SessionsController < ApplicationController
     @user = User.new
   end
 
-  # def create
-  # 	if params[:name].nil? || params[:name].empty?
-  #     	redirect_to :login
-  #   elsif params[:passowrd].nil? || params[:password].empty?
-  #   		redirect_to :login
-  #   else
-		#     @user = User.find_by(name: params[:name])
-		#     return head(:forbidden) unless @user.authenticate(params[:password])
-		#     session[:user_id] = @user.id
-		#     redirect_to '/'
-		# end
-  # end
-
   def create
     @user = User.find_by(name: params[:user][:name])
     if @user && @user.authenticate(params[:user][:password])
