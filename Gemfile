0546f700f2321d3d2ec547cc6cc38a403af4b568
yuanxizhang <yuanxi.zhang@gmail.com>
2019-04-08 21:42:52 -0700

    Done.

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.1.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
+gem "bcrypt", "~> 3.1.7"
 # gem 'therubyracer', platforms: :ruby
 gem 'bootsnap'
 # Use jquery as the JavaScript library
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,39 @@
+class SessionsController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  # def create
+  # 	if params[:name].nil? || params[:name].empty?
+  #     	redirect_to :login
+  #   elsif params[:passowrd].nil? || params[:password].empty?
+  #   		redirect_to :login
+  #   else
+		#     @user = User.find_by(name: params[:name])
+		#     return head(:forbidden) unless @user.authenticate(params[:password])
+		#     session[:user_id] = @user.id
+		#     redirect_to '/'
+		# end
+  # end
+
+  def create
+    @user = User.find_by(name: params[:user][:name])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to :welcome
+    else
+      flash[:notice] = "Login is incorrect"
+      redirect_to :login
+    end
+  end
+
+  def destroy
+    if current_user
+      session.delete :name
+    else
+      redirect_to '/login'
+
+    end
+  end
+end
