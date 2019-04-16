f988222ec7d98c20085b5bcb3171917c03b5156f
brennanmrose <brennan.m.rose@gmail.com>
2019-04-15 21:30:26 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  # resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#welcome'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get '/login', to: 'users#login'
+  post '/login', to: 'users#login'
+  get '/sessions', to: 'sessions#create'
 end
