44c9b4a974fb3be099f182266a57e731aa577c3b
Ashley <ashleywong678@gmail.com>
2019-04-25 16:10:40 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,14 @@
 class SessionsController < ApplicationController
 
   def create
-    @user = User.find_or_create(params)
+    @user = User.find_or_create_by(name: params[:user][:name])
+    return head(:forbidden) unless @user.authenticate(params[:user][:password])
     session[:user_id] = @user.id
   end
 
+  private
+
+  def session_params
+    params.require(:session).permit(:user_id)
+  end
 end
