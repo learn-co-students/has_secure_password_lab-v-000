4ad993253d5633d0ec012689ebdd43e77b101d16
AlwinaO <aoyewole2586@gmail.com>
2019-05-27 18:26:59 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,17 @@ class SessionsController < ApplicationController
 
   def create
 
+    user = User.find_by(name: params[:user][:name])
+
+    user = user.try(:authenticate, params[:user][:password])
+    return redirect_to login_path unless user
+
+    login(user)
+
+    @user = user
+
+    redirect_to welcome_path
+
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  # def log_in(user)
+  #   session[:user_id] = user.id
+  #
+  # end
+  #
+  # helper_method :log_in
 end
