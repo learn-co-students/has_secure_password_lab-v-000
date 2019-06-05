e035e62aa1434987e1244aba18b818186c2dca1b
Meg Gutshall <meghan.gutshall@gmail.com>
2019-06-05 16:58:08 -0400

    Edit controllers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,23 @@
 class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    if params[:name] == nil || params[:name].empty?
+      redirect_to login_path
+    else
+      @user = User.find_by(name: params[:name])
+      if @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect_to users_path
+      else
+        redirect_to login_path
+      end
+    end
+  end
+
+  def destroy
+    session.delete :name
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,18 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
   
+  def create
+    @user = User.create(user_params)
+  end
+
+  def show
+    @user = User.find_by_id(session[:user_id])
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:username, :password, :password_confirmation)
+    end
 end
