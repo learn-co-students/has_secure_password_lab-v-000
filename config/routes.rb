fe6fbc6f1f3874f5cf7feee55d677fdc3dea0a9a
Meg Gutshall <meghan.gutshall@gmail.com>
2019-06-05 16:58:18 -0400

    Draw routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'users#show'
+  post '/', to: 'users#show'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  post '/users', to: 'users#index'
+
 end
