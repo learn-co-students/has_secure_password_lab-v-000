ccb7bf070c8656c4da333ca2104aad02de0d9da3
Monica Marziano <mmarziano1@gmail.com>
2019-07-30 16:58:50 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,34 @@
 class UsersController < ApplicationController
+    helper_method :login
 
     def new
         @user = User.new 
     end 
 
     def create 
-      @user = User.new(user_params)
-        if @user.password.present? && @user.password == @user.password_confirmation
-            @user.save
-        end 
-      if @user.save 
-        @user.login
-        redirect_to user_path(@user)
-      else
-        redirect_to new_user_path
-      end 
-
+        @user = User.new(user_params)
+        if @user.save
+          session[:user_id] = @user.id
+          redirect_to :welcome
+        else
+          redirect_to :signup
+        end
+    
     end 
+
+    def show
+       @user = User.find_by(id: session[:user_id])
+    end 
+    
         
 private 
     def user_params
         params.require(:user).permit(:name, :password, :password_confirmation)
     end 
 
-    def login
-        if @user && @user.authenticate(params[:password])
-            session[:user_id] = @user.id 
+    def login(user)
+        if user && user.authenticate(@user.password)
+            session[:user_id] = user.id 
         else 
             flash[:message] = "Unable to login"
         end 
