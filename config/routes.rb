bbe1f24ea26cb1b496afbc087d2ba56662c85204
NaKia Whitby <whitby.nakia@gmail.com>
2019-08-19 23:33:05 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/signup', to: 'users#new'
+ get '/signup', to: 'users#new'
  post '/signup', to: 'users#create'
  get '/login', to: 'sessions#new'
  post '/login', to: 'sessions#create'
