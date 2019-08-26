81ceae5a68baf0712accbbd771a340d57580f117
Sushi Chanrai <sushi@pathworks.co>
2019-08-26 19:59:15 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,13 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
-    return redirect_to '/users/new' unless @user.save #read up on why return is necessary
+    return redirect_to '/signup' unless @user.save #read up on why return is necessary
     session[:user_id] = @user.id
     redirect_to users_welcome_path
   end
 
   def welcome
-    
+    @user = User.find(session[:user_id])
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   #get '/login' => 'sessions#new'
   #post '/login' => 'sessions#create'
-  resources :users, only: [:new, :create, :destroy, :index]
+  resources :users, only: [:create, :destroy, :index]
+  get '/signup', to: 'users#new'
   get 'users/welcome'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
