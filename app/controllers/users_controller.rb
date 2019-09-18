48c3f40aec5e8676c0296eb67a900651252fde39
Thomas Gray <thomas.gray@tgray.fios-router.home>
2019-09-17 20:24:46 -0400

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    if params[:user][:password] == params[:user][:password_confirmation]
+      @user = User.create(user_params)
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      redirect_to new_user_path, alert: "Invalid password."
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :password, :password_confirmation)
+    end
+end
