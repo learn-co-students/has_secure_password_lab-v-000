5d34b7d624478ed7f7775325bc5cc97b765a31df
Richard-Burd <qalbalassadalasfour@gmail.com>
2019-10-17 11:43:58 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  post 'users/new'
+  post 'sessions/create'
+  post 'users/create'
+  get 'welcome/homepage'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
