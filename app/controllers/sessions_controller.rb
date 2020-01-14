1ae78a1a0cb99ca42cad8a9cff68f99fa03748fb
sdcro <sdcrouse91@gmail.com>
2020-01-14 00:53:00 -0800

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,12 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+  end
+
+  def destroy
+    # For the fun of it, make a flash message here that tells users when they've successfully logged out.
+    # I will need to display it in the application.html.erb file if I do this.
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  root 'welcome#welcome'
+
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
