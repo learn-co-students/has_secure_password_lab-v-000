992810dc6cc90b9e6eb43dcc878be9a04383b8c8
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-03-09 16:27:53 -0400

    Done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,23 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create(user_params)
-    session[:user_id] = @user.id
-    redirect_to '/'
+    if password_match
+      @user = User.create(user_params)
+      session[:user_id] = @user.id
+      redirect_to '/'
+    else
+      redirect_to '/signup'
+    end
   end
 
   def user_params
     params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+
+  private
+
+  def password_match
+    params[:user][:password].present? && params[:user][:password_confirmation].present? &&
+      params[:user][:password] == params[:user][:password_confirmation]
   end
 end
