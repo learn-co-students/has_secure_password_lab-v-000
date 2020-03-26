b13ea3762ee1752e3bb48cde1cf3a35f70015dd7
github username <github email address>
2020-03-26 16:29:21 -0500

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,8 @@ class UsersController < ApplicationController
     end
 
     def create
-        @user = User.new
         if params[:user][:password] == params[:user][:password_confirmation] 
-            @user.name = params[:user][:name]
-            @user.password = params[:user][:password]
-            @user.password_confirmation = params[:user][:password_confirmation]
-            @user.save
+            @user = User.create(user_params)
             session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
