d09bef6c94930c287a642f6cbdd64e44b7b14451
Tyler Sell <tylerselldesign@gmail.com>
2020-04-10 20:21:53 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  
+  def new 
+  end 
+  
+  def create
+    user = User.find_by(name: params[:user][:name])
+    user = user.try(:authenticate, params[:user][:password])
+    
+    if user 
+      session[:user_id] = user.id 
+      redirect_to controller: 'welcome', action: 'home'
+    else 
+      redirect_to controller: 'sessions', action: 'new'
+    end 
+    
+  end 
+  
+  def destroy
+    session.delete :user_id
+  end 
+  
+end
