55cf1c8454f8239424060cbb0dcb15e1bdf3c002
tscotce <tatyana.celovsky@gmail.com>
2020-05-21 22:31:01 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :users
+  get '/users/welcome' => 'users#welcome'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
