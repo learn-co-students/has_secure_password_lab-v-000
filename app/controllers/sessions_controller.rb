3b17d41b6bb4fdfbd5e5ab3710859eb767b3fbb3
Sheena Bonilla <sheena.myb@gmail.com>
2020-07-07 03:22:13 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,5 +5,6 @@ class SessionsController < ApplicationController
 
   def create
     session[:name] = params[:name]
+    redirect_to '/'
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200707024254) do
+ActiveRecord::Schema.define(version: 20200707030452) do
 
   create_table "users", force: :cascade do |t|
     t.string   "new"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
