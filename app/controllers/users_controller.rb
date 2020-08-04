8cf04e105bceef9266a0a2a5794f4dcc30c9e779
Sheena Bonilla <sheena.myb@gmail.com>
2020-08-04 04:56:23 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
+    if @user.valid?
+      @user.save
       session[:id] = @user.id
       redirect_to :show
     else
