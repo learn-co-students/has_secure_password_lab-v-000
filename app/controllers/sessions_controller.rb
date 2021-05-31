93128135df6a461a46d5819549c94255ffdb1bba
Lamia Haider <lamia.haider@gmail.com>
2021-05-30 21:48:19 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+  
+  def new
+    end
+#params nested under user!
+  def create  
+    if user_params[:password] == nil || user_params[:password].empty?
+      redirect_to '/login'
+    else
+      @user = User.find_by(name: user_params[:name])
+      if @user.authenticate(user_params[:password])
+        session[:user_id] = @user.id
+        redirect_to '/welcome'
+      else
+        redirect_to '/login'
+      end
+    end
+end
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :password)
+    end
+            
+
+end
